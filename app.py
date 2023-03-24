4e48f1cdff2904ac2515a149031bf501c31f4d72
ahmexxx <ahmedibralav@gmail.com>
2023-03-24 12:39:22 +0100

    asd

--- app.py
+++ app.py
@@ -6,27 +6,27 @@ print('funziona?')
 
 st.title('HEYHEY')
 st.title('prima _APP_ :blue[CHE BELLO] emojis :sunglasses:')
-# #import streamlit as st
-# #st.text("ciaoooooooo front")
-# ##
-# #st.write('1 + 1 = ', 2)
-# #st.write('Below is a DataFrame:', data_frame, 'Above is a dataframe.')
-# ##
-# #import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import altair as alt
+#import streamlit as st
+#st.text("ciaoooooooo front")
+##
+#st.write('1 + 1 = ', 2)
+#st.write('Below is a DataFrame:', data_frame, 'Above is a dataframe.')
+##
+#import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
 
-# df = pd.DataFrame(
-#     np.random.randn(200, 3),
-#     columns=['a', 'b', 'c'])
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
 
-# c = alt.Chart(df).mark_circle().encode(
-#     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-# #
-# from PIL import Image
-# img = Image.open('sun.jpg')
-# st.image(img, caption='sunrise')
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+#
+from PIL import Image
+img = Image.open('sun.jpg')
+st.image(img, caption='sunrise')
 
 
 
